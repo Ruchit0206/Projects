c32bf258cd8445ef41664ab66ad13f5fe9cfb352
Ruchit0206 <ruchit0206@gmail.com>
2024-07-23 22:30:15 +0530

    new channges added

--- Dictionary/Currency Converter/index.js
+++ Dictionary/Currency Converter/index.js
@@ -1,12 +1,9 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/";
-
- 
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
-
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
@@ -35,27 +32,21 @@ const updateExchangeRate = async () => {
     amtVal = 1;
     amount.value = "1";
   }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
-
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
   let response = await fetch(URL);
   let data = await response.json();
-  console.log(data);
   let rate = data[toCurr.value.toLowerCase()];
 
   let finalAmount = amtVal * rate;
-  
   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 };
 
 const updateFlag = (element) => {
   let currCode = element.value;
-  console.log(currCode);
   let countryCode = countryList[currCode];
-  console.log(countryCode)
   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
-  // console.log(fromCurr)
 };
 
 btn.addEventListener("click", (evt) => {
